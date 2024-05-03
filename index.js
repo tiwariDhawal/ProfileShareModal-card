8e09b9e85c297288e13e899de1c8173e86e0422b
Dhawal Tiwari <98008402+tiwariDhawal@users.noreply.github.com>
2024-05-03 09:26:36 +0530

    InitialCommit

---
+++ index.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+//Modal open function
+const openModal = () => {
+  console.log("Modal is open");
+  modal.classList.add("active");
+  overlay.classList.add("overlayactive");
+};
+
+//Close modal function
+const closeModal = () => {
+  console.log("Modal is closed");
+  modal.classList.remove("active");
+  overlay.classList.remove("overlayactive");
+};
